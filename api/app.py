c3528e0050649dd6e2efa5e91f2352832ce1956a
MartinianoOjeda <89236488+MartinianoOjeda@users.noreply.github.com>
2023-07-31 19:53:49 -0300

    incorporado app.static_folder toma el css y js

--- api/app.py
+++ api/app.py
@@ -5,6 +5,7 @@ from views import router
 app = Flask(__name__)
 app.secret_key = "secret key"
 app.register_blueprint(router)
+app.static_folder = "static" ### read css and js files
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=settings.API_DEBUG)
